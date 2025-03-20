4a81abe2d0e2daac7b0f088cbbe67df3c3069677
capprinha <gutoklein21@gmail.com>
2025-03-20 19:28:31 -0300

    mais conteudo sobre objetos

--- main.js
+++ main.js
@@ -9,8 +9,37 @@ function Carro(modelo, fabricante, anoModelo, anoFabricacao){
     this.batido = function(){
         console.log('bateu o carro')
     }
+} // função construtora
+
+const palio = new Carro('Palio', 'Fiat', 2005, 2004) // herança
+
+const nome = "luis"
+const idade = 21
+const maiorDe18 = true
+const conhecimento = ['python', 'js', 'sass']
+
+const pessoa = {
+    nomeDaPessoa: nome,
+    idadeDaPessoa : idade,
+    maiorDeIdade : maiorDe18,
+    conhecimentos : conhecimento
+} // objeto comum 
+
+console.log(typeof nome) // retorna o tipo do dado : str, int , float , true, false, object
+console.log(palio instanceof Carro) // verifica se 'palio' é uma instancia de Carro
+console.log(pessoa instanceof Carro) // não é uma instancia de Carro 
+
+pessoa.sobrenome = 'cappra' //add chave e valor / ['sobrenome']
+
+if ('sobrenome' in pessoa){
+    console.log('a pessoa tem sobrenome')
 }
 
-const palio = new Carro('Palio', 'Fiat', 2005, 2004)
+Object.freeze(pessoa) // usado para congelar os valores do objeto / sem alterar
 
-console.log(palio)
+pessoa.nome = 'augusto' // objeto congelado ao tentar mudar o valor da chave retorna undefined
+
+console.log(pessoa.nome)
+
+console.log(Object.keys(pessoa)) // retorna as chaves do objeto em array // .length
+console.log(Object.values(pessoa)) // retorna os valores das chaves do objeto em array
